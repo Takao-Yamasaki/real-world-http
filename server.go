cc7852def134e59b441970c118d37f0e053a830c
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-01-06 09:40:07 +0900

    add:cookies

--- server.go
+++ server.go
@@ -4,18 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
+	// "net/http/httputil"
 )
 
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	// リクエストに含まれる情報をテキストとして画面出力
+// 	dump, err := httputil.DumpRequest(r, true)
+// 	if err != nil {
+// 		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	fmt.Println(string(dump))
+// 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+// }
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	// リクエストに含まれる情報をテキストとして画面出力
-	dump, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	// レスポンスヘッダーにCookieが存在すれば、訪問したことがある人
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>初回訪問</body></html>\n")
 	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
 // httpサーバーを初期化
