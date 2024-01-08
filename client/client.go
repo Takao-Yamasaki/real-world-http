16c77110f1086c3b24fbde3624bd07c737176cfb
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-01-08 23:34:04 +0900

    add:post

--- client/client.go
+++ client/client.go
@@ -3,18 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/url"
+	// "os"
+	"strings"
 )
 
+// ファイルを渡す場合
+// func main() {
+// 	file, err := os.Open("client.go")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	resp, err := http.Post("http://server:18888", "text/plain", file)
+// 	if err != nil {
+// 		// 送信失敗
+// 		panic(err)
+// 	}
+// 	log.Println("Status:", resp.Status)
+// }
+
+// プログラム中で生成したテキストを渡す場合
 func main() {
-	// x-www-form-urlencoded形式のPOSTメソッド
-	values := url.Values{
-		"test": {"value"},
-	}
-	resp, err := http.PostForm("http://server:18888", values)
+	reader := strings.NewReader("テキスト")
+	resp, err := http.Post("http://server:18888", "text/plain", reader)
 	if err != nil {
+		// 送信失敗
 		panic(err)
 	}
 	log.Println("Status:", resp.Status)
-	log.Println("StatusCode:", resp.StatusCode)
 }
